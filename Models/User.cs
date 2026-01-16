7736652f1286a60be546e2f14b0066b9e7b6ee86
marquesmaycon <mayconmarquesh@gmail.com>
2026-01-15 23:57:12 -0300

    rf: required props, default createdAt

--- Models/User.cs
+++ Models/User.cs
@@ -1,20 +1,15 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace CSharpApi.Models
 {
     public class User
     {
         public int Id { get; set; }
-        
-        public string Name { get; set; }
-        
-        public string Email { get; set; }
 
-        public string Password { get; set; }
+        public required string Name { get; set; }
 
-        public DateTime CreatedAt { get; set; }
+        public required string Email { get; set; }
+
+        public required string Password { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     }
 }
